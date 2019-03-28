fc00357ca0d822862db2834c4df8ae364510e745
GomezOrtiz <david.gomez.mail@gmail.com>
2019-03-29 00:09:09 +0100

    Some provisional styling

--- server/routes/index.js
+++ server/routes/index.js
@@ -63,10 +63,10 @@ router.post("/writeFile", (req,res,next) => {
 
   let spec = content + `; describe("A ver si funcionase esto...", () => {
 
-    it("debería funcionar", () => {
+    it("La suma de 2 y 4 es 6", () => {
       expect(suma(2,4)).toEqual(6)
     })
-    it("no debería funcionar", () => {
+    it("La suma de 3 y 2 no es 4", () => {
         expect(suma(3,2)).toEqual(4)
     })
   })` 

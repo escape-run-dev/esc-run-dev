c8113102815e059dd2e60fa6645413842054a328
joseocamar <joseocamar@gmail.com>
2019-04-01 09:45:52 +0200

    Realizando N.Cage

---
+++ client/src/components/Cage.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react'
+
+class Cage extends Component {
+
+  constructor(){
+    super()
+    this.state = {
+      class: "Cage"
+    }
+  }
+
+  // handlers
+
+  render() {
+    
+    return (
+
+      <main>
+        <section>
+          <h1>Obreros calientes trabajando en ello</h1>
+        
+          <form>
+            <input type="text"></input>
+            <button></button>
+          </form>  
+        
+        </section>
+          <h1>Imagenes picantes de N.Cage</h1>
+        <section>
+
+        </section>
+      </main>
+    )
+
+  }
+
+
+}
+
+
+default export Cage

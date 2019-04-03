85132b56cc1fe4d56a8dc02cb256d41056910eed
GomezOrtiz <david.gomez.mail@gmail.com>
2019-04-03 16:17:05 +0200

    Ready to Heroku

--- client/src/components/canvas/collisions.js
+++ client/src/components/canvas/collisions.js
@@ -3,7 +3,8 @@
         return (
         ((game.player.x + game.player.w) >= obstacle.x &&
             game.player.x < (obstacle.x + obstacle.w) &&
-            game.player.y + (game.player.h - 20) >= obstacle.y)
+         	game.player.y <= obstacle.y &&
+            game.player.y + (game.player.h -20) >= obstacle.y)
         )
     })
 }

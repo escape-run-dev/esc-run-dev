69e08f33fbe85ea0d3bc3617e74a85f7bccef026
GomezOrtiz <david.gomez.mail@gmail.com>
2019-04-04 00:49:56 +0200

    Trying

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
 
+rm -rf server/public/*
+rm -rf client/build
+cd client 
+npm run build-prod
+cd ../
+mv client/build/* server/public
+git add .
+git commit -m "Deploying"
+git subtree push --prefix=server heroku master
+heroku logs --tail
